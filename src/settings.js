e5da4834bbf8bb3d1e7b25d58f77767ba500b48f
JMDonna <zjmqinger@gmail.com>
2021-12-29 16:39:27 +0800

    upload

---
+++ src/settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // 系统名称
+  title: "系统demo",
+  // 界面设置框
+  showSettings: true,
+  // 导航tag
+  tagsView: true,
+  // 固定header
+  fixedHeader: false,
+  // logo展示
+  sidebarLogo: true,
+
+  errorLog: "production",
+};
